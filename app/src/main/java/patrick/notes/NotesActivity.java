c4cd3379f7fb8882c7a377dc73b3b1deb33b276a
Patrick Gartenmann <gartenmannpatrick@gmx.ch>
2018-10-06 20:08:20 +0200

    Update to Android 8.1
    Update to Android 8.1
    Fix no text save

--- app/src/main/java/patrick/notes/NotesActivity.java
+++ app/src/main/java/patrick/notes/NotesActivity.java
@@ -3,6 +3,7 @@ package patrick.notes;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -47,7 +48,9 @@ public class NotesActivity extends AppCompatActivity {
         btnRestoreData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onInsert();
+                if(!String.valueOf(tv.getText()).equals("")){
+                    onInsert();
+                }
                 Intent intent = new Intent(NotesActivity.this, SavesActivity.class);
                 startActivity(intent);
             }
